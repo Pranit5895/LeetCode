898eec156de907a228032e34dfc9f0583afa5cab
Pranit5895 <guptapranit9@gmail.com>
2021-07-04 16:45:51 +0530

    Method 4'

--- 1. Two-Sum.cpp
+++ 1. Two-Sum.cpp
@@ -145,3 +145,33 @@ public:
     }
 };
 
+
+/**
+Approach 3: One-pass Hash Table
+build and check for complement at the same time
+**/
+
+/**
+Complexity Analysis:
+Time complexity : O(n).
+We traverse the list containing nn elements only once. 
+Each look up in the table costs only O(1) time.
+Space complexity : O(n). 
+The extra space required depends on the number of items stored in the hash table, which stores at most nn elements.
+**/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> ixs;
+        for(int i = 0; i < nums.size(); i++){
+            int complement = target - nums[i];
+            if(ixs.find(complement) != ixs.end()){
+                return vector<int> {i, ixs[complement]};
+            }
+            ixs[nums[i]] = i;
+        }
+        return vector<int> {};
+    }
+};
+
