7fd9ec14328f1ff6539e01f22b271be56996581c
Pranit5895 <guptapranit9@gmail.com>
2021-07-07 19:40:10 +0530

    Approach 2

--- 9. Palindrome-Number.cpp
+++ 9. Palindrome-Number.cpp
@@ -12,3 +12,36 @@ public:
         return true;
     }
 };
+
+/**
+Approach 1: Revert half of the number
+**/
+
+/**
+Complexity Analysis
+Time complexity : O(\log_{10}(n)). We divided the input by 10 for every iteration, so the time complexity is O(\log_{10}(n))
+Space complexity : O(1).
+**/
+
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        //if x is negative, there will be a '-' sign
+        //if x's last digit is 0, then x must be 0
+        if(x < 0 || (x != 0 && x%10 == 0)) return false;
+        
+        int reverted = 0;
+        //construct reverted number of last half of x
+        //do until reverted >= x
+        while(x > reverted){
+            reverted = reverted*10 + x%10;
+            x/=10;
+        }
+        
+        //if x has odd digits, 
+        //we need to use /10 to get rid of the middle digit
+        //(which is last digit in reverted)
+        return x==reverted || x==reverted/10;
+    }
+};
