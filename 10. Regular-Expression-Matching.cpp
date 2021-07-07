eef60662e0a59684e93b62a3e810a916e26ef438
Pranit5895 <guptapranit9@gmail.com>
2021-07-07 19:44:32 +0530

    10. Regular Expression Matching

--- 10. Regular-Expression-Matching.cpp
+++ 10. Regular-Expression-Matching.cpp
@@ -4,30 +4,38 @@
 //Memory Usage: 13.5 MB, less than 11.87% of C++ online submissions for Regular Expression Matching.
 //time: O((T+P)*2^(T+P/2))?
 //space: O(T^2+P^2)?
-class Solution {
+class Solution
+{
 public:
-    bool isMatch(string s, string p) {
-        if(p.size() == 0) return s.size() == 0;
-        
+    bool isMatch(string s, string p)
+    {
+        if (p.size() == 0)
+            return s.size() == 0;
+
         // cout << p << " " << s << endl;
-        
+
         //edge case
-        if(s.size() == 0){
+        if (s.size() == 0)
+        {
             // return p.size() == 2 && p[1] == '*';
             //c*c*
             //when s is empty, p can match s only it's a combination of "?*"
-            if(p.size() % 2 != 0) return false;
-            for(int i = 1; i < p.size(); i+=2){
-                if(p[i] != '*') return false;
+            if (p.size() % 2 != 0)
+                return false;
+            for (int i = 1; i < p.size(); i += 2)
+            {
+                if (p[i] != '*')
+                    return false;
             }
             return true;
         }
-        
+
         //here s and p are both not empty
-        
+
         bool first_match = (p[0] == '.' || p[0] == s[0]);
-        
-        if(p.size() >= 2 && p[1] == '*'){
+
+        if (p.size() >= 2 && p[1] == '*')
+        {
             /*
             discover '*'
             either match the first 2 char of s or not match
@@ -37,8 +45,10 @@ public:
             for the second case: ignore the x* in p
             */
             return (first_match && isMatch(s.substr(1), p)) ||
-                isMatch(s, p.substr(2));
-        }else{
+                   isMatch(s, p.substr(2));
+        }
+        else
+        {
             /*
             when discovering '*', we must consider the first 2 char of p together,
             so we should not go here
@@ -51,13 +61,16 @@ public:
 //recursion, no edge case
 //Runtime: 596 ms, faster than 12.34% of C++ online submissions for Regular Expression Matching.
 //Memory Usage: 13.6 MB, less than 11.87% of C++ online submissions for Regular Expression Matching.
-class Solution {
+class Solution
+{
 public:
-    bool isMatch(string s, string p) {
-        if(p.size() == 0) return s.size() == 0;
-        
+    bool isMatch(string s, string p)
+    {
+        if (p.size() == 0)
+            return s.size() == 0;
+
         // cout << p << " " << s << endl;
-        
+
         // //edge case
         // if(s.size() == 0){
         //     // return p.size() == 2 && p[1] == '*';
@@ -69,14 +82,15 @@ public:
         //     }
         //     return true;
         // }
-        
+
         //here s and p are both not empty
-        
+
         //we should check whether s is not empty!
         //so we don't need the handle of edge case above
         bool first_match = (s.size() > 0) && (p[0] == '.' || p[0] == s[0]);
-        
-        if(p.size() >= 2 && p[1] == '*'){
+
+        if (p.size() >= 2 && p[1] == '*')
+        {
             /*
             discover '*'
             either match the first 2 char of s or not match
@@ -86,8 +100,10 @@ public:
             for the second case: ignore the x* in p
             */
             return (first_match && isMatch(s.substr(1), p)) ||
-                isMatch(s, p.substr(2));
-        }else{
+                   isMatch(s, p.substr(2));
+        }
+        else
+        {
             /*
             when discovering '*', we must consider the first 2 char of p together,
             so we should not go here
@@ -101,47 +117,59 @@ public:
 //https://github.com/keineahnung2345/fucking-algorithm/blob/note/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E7%B3%BB%E5%88%97/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E4%B9%8B%E6%AD%A3%E5%88%99%E8%A1%A8%E8%BE%BE.md
 //Runtime: 4 ms, faster than 95.00% of C++ online submissions for Regular Expression Matching.
 //Memory Usage: 7 MB, less than 100.00% of C++ online submissions for Regular Expression Matching.
-class Solution {
+class Solution
+{
 public:
     string s, p;
     vector<vector<int>> memo;
-    
-    bool dfs(int i, int j){
+
+    bool dfs(int i, int j)
+    {
         //i for s, j for p
-        if(j == p.size()) return i == s.size();
-        
-        if(memo[i][j] != -1){
+        if (j == p.size())
+            return i == s.size();
+
+        if (memo[i][j] != -1)
+        {
             return memo[i][j];
         }
-        
+
         // cout << i << " " << j << endl;
-        
+
         //edge case
-        if(i == s.size()){
+        if (i == s.size())
+        {
             //(p.size()-j): remaining size
-            if((p.size()-j) % 2 != 0) return false;
-            for(int k = j+1; k < p.size(); k+=2){
-                if(p[k] != '*') return false;
+            if ((p.size() - j) % 2 != 0)
+                return false;
+            for (int k = j + 1; k < p.size(); k += 2)
+            {
+                if (p[k] != '*')
+                    return false;
             }
             return true;
         }
-        
+
         bool first_match = (p[j] == '.' || p[j] == s[i]);
-        
-        if(j+1 < p.size() && p[j+1] == '*'){
-            memo[i][j] = (first_match && dfs(i+1, j)) ||
-                dfs(i, j+2);
-        }else{
-            memo[i][j] =  first_match && dfs(i+1, j+1);
+
+        if (j + 1 < p.size() && p[j + 1] == '*')
+        {
+            memo[i][j] = (first_match && dfs(i + 1, j)) ||
+                         dfs(i, j + 2);
         }
-        
+        else
+        {
+            memo[i][j] = first_match && dfs(i + 1, j + 1);
+        }
+
         return memo[i][j];
     }
-    
-    bool isMatch(string s, string p) {
+
+    bool isMatch(string s, string p)
+    {
         this->s = s;
         this->p = p;
-        memo = vector<vector<int>>(s.size()+1, vector(p.size()+1, -1));
+        memo = vector<vector<int>>(s.size() + 1, vector(p.size() + 1, -1));
         return dfs(0, 0);
     }
 };
@@ -149,37 +177,45 @@ public:
 //recursion + memorization, no edge case
 //Runtime: 4 ms, faster than 95.05% of C++ online submissions for Regular Expression Matching.
 //Memory Usage: 7 MB, less than 100.00% of C++ online submissions for Regular Expression Matching.
-class Solution {
+class Solution
+{
 public:
     string s, p;
     vector<vector<int>> memo;
-    
-    bool dfs(int i, int j){
+
+    bool dfs(int i, int j)
+    {
         //i for s, j for p
-        if(j == p.size()) return i == s.size();
-        
-        if(memo[i][j] != -1){
+        if (j == p.size())
+            return i == s.size();
+
+        if (memo[i][j] != -1)
+        {
             return memo[i][j];
         }
-        
+
         // cout << i << " " << j << endl;
-        
+
         bool first_match = (i < s.size()) && (p[j] == '.' || p[j] == s[i]);
-        
-        if(j+1 < p.size() && p[j+1] == '*'){
-            memo[i][j] = (first_match && dfs(i+1, j)) ||
-                dfs(i, j+2);
-        }else{
-            memo[i][j] =  first_match && dfs(i+1, j+1);
+
+        if (j + 1 < p.size() && p[j + 1] == '*')
+        {
+            memo[i][j] = (first_match && dfs(i + 1, j)) ||
+                         dfs(i, j + 2);
         }
-        
+        else
+        {
+            memo[i][j] = first_match && dfs(i + 1, j + 1);
+        }
+
         return memo[i][j];
     }
-    
-    bool isMatch(string s, string p) {
+
+    bool isMatch(string s, string p)
+    {
         this->s = s;
         this->p = p;
-        memo = vector<vector<int>>(s.size()+1, vector(p.size()+1, -1));
+        memo = vector<vector<int>>(s.size() + 1, vector(p.size() + 1, -1));
         return dfs(0, 0);
     }
 };
@@ -188,14 +224,16 @@ public:
 //Runtime: 12 ms, faster than 54.45% of C++ online submissions for Regular Expression Matching.
 //Memory Usage: 6.6 MB, less than 100.00% of C++ online submissions for Regular Expression Matching.
 //time: O(mn), space: O(mn)
-class Solution {
+class Solution
+{
 public:
-    bool isMatch(string s, string p) {
+    bool isMatch(string s, string p)
+    {
         int m = s.size(), n = p.size();
         //dp[i,j] : substring starts from (i, j)
         //i 's range: [0, m], 0 means the whole string, m means empty string
-        vector<vector<bool>> dp(m+1, vector(n+1, false));
-        
+        vector<vector<bool>> dp(m + 1, vector(n + 1, false));
+
         //base case when both are empty string
         dp[m][n] = true;
         /*
@@ -203,22 +241,27 @@ public:
         that means s is not empty, but p is empty,
         that's always false
         */
-        
-        for(int i = m; i >= 0; i--){
+
+        for (int i = m; i >= 0; i--)
+        {
             //dp[i][n] are already set
-            for(int j = n-1; j >= 0; j--){
+            for (int j = n - 1; j >= 0; j--)
+            {
                 // cout << "(" << i << ", " << j << ")" << endl;
                 bool first_match = (i < m) && (p[j] == '.' || p[j] == s[i]);
-                
-                if(j+1 < n && p[j+1] == '*'){
+
+                if (j + 1 < n && p[j + 1] == '*')
+                {
                     //match first char or ignore "?*" in p
-                    dp[i][j] = (first_match && dp[i+1][j]) || dp[i][j+2];
-                }else{
-                    dp[i][j] = first_match && dp[i+1][j+1];
+                    dp[i][j] = (first_match && dp[i + 1][j]) || dp[i][j + 2];
+                }
+                else
+                {
+                    dp[i][j] = first_match && dp[i + 1][j + 1];
                 }
             }
         }
-        
+
         //whole string of s and whole string of p
         return dp[0][0];
     }
