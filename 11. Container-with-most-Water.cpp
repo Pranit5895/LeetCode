aaf0eac89bede7ebb00ec9c42be338aae5086d42
Pranit5895 <guptapranit9@gmail.com>
2021-07-07 19:45:59 +0530

    Approach 3

--- 11. Container-with-most-Water.cpp
+++ 11. Container-with-most-Water.cpp
@@ -51,3 +51,27 @@ public:
         return ans;
     }
 };
+
+//Approach 2: Two Pointer Approach
+//Runtime: 16 ms, faster than 96.00% of C++ online submissions for Container With Most Water.
+//Memory Usage: 9 MB, less than 100.00% of C++ online submissions for Container With Most Water.
+//time: O(n), space: O(1)
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int N = height.size();
+        int left = 0, right = N-1, ans = 0;
+        while(left < right){
+            ans = max(ans, min(height[left], height[right]) * (right-left));
+            //move the pointer pointing to lower bar forward,
+            //because if we move the pointer pointing to higher bar forward, 
+            //the area can only be shrinked
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return ans;
+    }
+};
