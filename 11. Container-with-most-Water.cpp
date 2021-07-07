9aa559e9928ea4b4d6be7a15b59fe6f017ebf85d
Pranit5895 <guptapranit9@gmail.com>
2021-07-07 19:46:18 +0530

    11. Container With Most Water

--- 11. Container-with-most-Water.cpp
+++ 11. Container-with-most-Water.cpp
@@ -1,35 +1,42 @@
 //WA
-class Solution {
+class Solution
+{
 public:
-    int maxArea(vector<int>& height) {
+    int maxArea(vector<int> &height)
+    {
         int N = height.size();
-        int left = 0, right = N-1;
+        int left = 0, right = N - 1;
         int ans = 0;
         bool leftTurn = true;
-        
-        while(left < right){
+
+        while (left < right)
+        {
             int lh = height[left], rh = height[right];
             // cout << left << " " << right << endl;
-            ans = max(ans, min(lh, rh) * (right-left));
-            
-            if(leftTurn){
+            ans = max(ans, min(lh, rh) * (right - left));
+
+            if (leftTurn)
+            {
                 //move left so heigh[left] is larger than old lh
-                do{
+                do
+                {
                     left++;
                     // cout << "left: " << left << endl;
-                }while(left < N && height[left] <= lh);
-            }else{
+                } while (left < N && height[left] <= lh);
+            }
+            else
+            {
                 //move right so height[right] is larger than old rh
-                do{
+                do
+                {
                     right--;
                     // cout << "right: " << right << endl;
-                }while(right >= 0 && height[right] <= rh);
+                } while (right >= 0 && height[right] <= rh);
             }
-            
+
             leftTurn = !leftTurn;
-            
         }
-        
+
         return ans;
     }
 };
@@ -37,17 +44,21 @@ public:
 //Approach 1: Brute Force
 //TLE
 //time: O(n^2), space: O(1)
-class Solution {
+class Solution
+{
 public:
-    int maxArea(vector<int>& height) {
+    int maxArea(vector<int> &height)
+    {
         int N = height.size();
         int ans = 0;
-        for(int i = 0; i < N-1; i++){
-            for(int j = i+1; j < N; j++){
-                ans = max(ans, min(height[i], height[j]) * (j-i));
+        for (int i = 0; i < N - 1; i++)
+        {
+            for (int j = i + 1; j < N; j++)
+            {
+                ans = max(ans, min(height[i], height[j]) * (j - i));
             }
         }
-            
+
         return ans;
     }
 };
@@ -56,19 +67,25 @@ public:
 //Runtime: 16 ms, faster than 96.00% of C++ online submissions for Container With Most Water.
 //Memory Usage: 9 MB, less than 100.00% of C++ online submissions for Container With Most Water.
 //time: O(n), space: O(1)
-class Solution {
+class Solution
+{
 public:
-    int maxArea(vector<int>& height) {
+    int maxArea(vector<int> &height)
+    {
         int N = height.size();
-        int left = 0, right = N-1, ans = 0;
-        while(left < right){
-            ans = max(ans, min(height[left], height[right]) * (right-left));
+        int left = 0, right = N - 1, ans = 0;
+        while (left < right)
+        {
+            ans = max(ans, min(height[left], height[right]) * (right - left));
             //move the pointer pointing to lower bar forward,
-            //because if we move the pointer pointing to higher bar forward, 
+            //because if we move the pointer pointing to higher bar forward,
             //the area can only be shrinked
-            if(height[left] < height[right]){
+            if (height[left] < height[right])
+            {
                 left++;
-            }else{
+            }
+            else
+            {
                 right--;
             }
         }
