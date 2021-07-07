5990d0e4025341e24e128d7974683fd675335897
Pranit5895 <guptapranit9@gmail.com>
2021-07-07 19:43:56 +0530

    Approach 4

--- 10. Regular-Expression-Matching.cpp
+++ 10. Regular-Expression-Matching.cpp
@@ -145,3 +145,41 @@ public:
         return dfs(0, 0);
     }
 };
+
+//recursion + memorization, no edge case
+//Runtime: 4 ms, faster than 95.05% of C++ online submissions for Regular Expression Matching.
+//Memory Usage: 7 MB, less than 100.00% of C++ online submissions for Regular Expression Matching.
+class Solution {
+public:
+    string s, p;
+    vector<vector<int>> memo;
+    
+    bool dfs(int i, int j){
+        //i for s, j for p
+        if(j == p.size()) return i == s.size();
+        
+        if(memo[i][j] != -1){
+            return memo[i][j];
+        }
+        
+        // cout << i << " " << j << endl;
+        
+        bool first_match = (i < s.size()) && (p[j] == '.' || p[j] == s[i]);
+        
+        if(j+1 < p.size() && p[j+1] == '*'){
+            memo[i][j] = (first_match && dfs(i+1, j)) ||
+                dfs(i, j+2);
+        }else{
+            memo[i][j] =  first_match && dfs(i+1, j+1);
+        }
+        
+        return memo[i][j];
+    }
+    
+    bool isMatch(string s, string p) {
+        this->s = s;
+        this->p = p;
+        memo = vector<vector<int>>(s.size()+1, vector(p.size()+1, -1));
+        return dfs(0, 0);
+    }
+};
