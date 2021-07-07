950e24b685ac2a1f6e147872bbbe6fe2c153cb54
Pranit5895 <guptapranit9@gmail.com>
2021-07-07 19:45:43 +0530

    Approach 2

--- 11. Container-with-most-Water.cpp
+++ 11. Container-with-most-Water.cpp
@@ -33,3 +33,21 @@ public:
         return ans;
     }
 };
+
+//Approach 1: Brute Force
+//TLE
+//time: O(n^2), space: O(1)
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int N = height.size();
+        int ans = 0;
+        for(int i = 0; i < N-1; i++){
+            for(int j = i+1; j < N; j++){
+                ans = max(ans, min(height[i], height[j]) * (j-i));
+            }
+        }
+            
+        return ans;
+    }
+};
