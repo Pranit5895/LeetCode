e14ee15f2d912b2d0d9c6fd5e7753c9a08acd7a4
Pranit5895 <guptapranit9@gmail.com>
2021-07-08 13:33:33 +0530

    Approach 3

--- 14. Longest-Common-Prefix.cpp
+++ 14. Longest-Common-Prefix.cpp
@@ -42,3 +42,39 @@ public:
         return strs[0];
     }
 };
+
+//Divide and Conquer
+//In the worst case we have nn equal strings with length m
+//Time complexity : O(S), where S is the number of all characters in the array, 
+//S=m⋅n Time complexity is 2⋅T(n/2)+O(m). Therefore time complexity is O(S). 
+//In the best case this algorithm performs O(minLen⋅n) comparisons, where minLen is the shortest string of the array.
+//Space complexity : O(m⋅logn)
+//There is a memory overhead since we store recursive calls in the execution stack. 
+//There are logn recursive calls, each store need m space to store the result, so space complexity is O(m⋅logn)
+//Runtime: 8 ms, faster than 98.47% of C++ online submissions for Longest Common Prefix.
+//Memory Usage: 12 MB, less than 11.34% of C++ online submissions for Longest Common Prefix.
+
+class Solution {
+public:
+    string commonPrefix(string left, string right){
+        int _min = min(left.size(), right.size());
+        for(int i = 0; i < _min; i++){
+            if(left[i] != right[i]){
+                //i is the length of the resulting substring
+                return left.substr(0, i);
+            }
+        }
+        return left.substr(0, _min);
+    }
+    string longestCommonPrefix(vector<string>& strs, int l, int r){
+        if(l == r) return strs[l];
+        int mid = (l+r)/2;
+        string lcpL = longestCommonPrefix(strs, l, mid);
+        string lcpR = longestCommonPrefix(strs, mid+1, r);
+        return commonPrefix(lcpL, lcpR);
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size() == 0) return "";
+        return longestCommonPrefix(strs, 0, strs.size()-1);
+    }
+};
