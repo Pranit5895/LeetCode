af2139f7ebddf7b0d7997ca9f1818ec3b7b50de5
Pranit5895 <guptapranit9@gmail.com>
2021-07-08 13:33:12 +0530

    Approach 2

--- 14. Longest-Common-Prefix.cpp
+++ 14. Longest-Common-Prefix.cpp
@@ -19,3 +19,26 @@ public:
         return strs[0].substr(0, ans);
     }
 };
+
+//Vertical scanning
+//Time complexity : O(S) , where S is the sum of all characters in all strings. 
+//In the worst case there will be nn equal strings with length mm and the algorithm performs S=m⋅n character comparisons. 
+//Even though the worst case is still the same as Approach 1, 
+//in the best case there are at most n⋅minLen comparisons where minLen is the length of the shortest string in the array.
+//Space complexity : O(1). We only used constant extra space. 
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size() == 0) return "";
+        for(int i = 0; i < strs[0].size(); i++){
+            //i: index of char
+            for(int j = 1; j < strs.size(); j++){
+                //j: index of str
+                if(i >= strs[j].size() || strs[0][i] != strs[j][i]){
+                    return strs[0].substr(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+};
