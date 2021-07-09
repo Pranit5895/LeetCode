5b0094a3abedc0975ffd8fa7fa34238f90def6da
Pranit5895 <guptapranit9@gmail.com>
2021-07-09 17:08:43 +0530

    17. Letter Combinations of a Phone Number

--- 17. Letter-Combinations-of-a-Phone-Number.cpp
+++ 17. Letter-Combinations-of-a-Phone-Number.cpp
@@ -1,12 +1,15 @@
 //Runtime: 0 ms, faster than 100.00% of C++ online submissions for Letter Combinations of a Phone Number.
 //Memory Usage: 6.8 MB, less than 34.49% of C++ online submissions for Letter Combinations of a Phone Number.
-class Solution {
+class Solution
+{
 public:
-    vector<string> letterCombinations(string digits) {
-        if(digits.empty()) return vector<string>();
-        
+    vector<string> letterCombinations(string digits)
+    {
+        if (digits.empty())
+            return vector<string>();
+
         vector<string> digit2letters(10);
-        
+
         digit2letters[2] = "abc";
         digit2letters[3] = "def";
         digit2letters[4] = "ghi";
@@ -15,20 +18,23 @@ public:
         digit2letters[7] = "pqrs";
         digit2letters[8] = "tuv";
         digit2letters[9] = "wxyz";
-        
+
         vector<string> ans = {""};
-        
-        for(const char& c : digits){
-            int d = c-'0';
+
+        for (const char &c : digits)
+        {
+            int d = c - '0';
             vector<string> olds = ans;
             ans.clear();
-            for(const string& old : olds){
-                for(const char& c : digit2letters[d]){
-                    ans.push_back(old+c);
+            for (const string &old : olds)
+            {
+                for (const char &c : digit2letters[d])
+                {
+                    ans.push_back(old + c);
                 }
             }
         }
-        
+
         return ans;
     }
 };
@@ -38,24 +44,31 @@ public:
 //Memory Usage: 6.8 MB, less than 36.51% of C++ online submissions for Letter Combinations of a Phone Number.
 //N: number of digits in "digits" mapping to 3 chars, M: number of digits in "digits" mapping to 4 chars
 //time: O(3^N * 4^M), space: O(3^N * 4^M)
-class Solution {
+class Solution
+{
 public:
     vector<string> digit2letters;
     vector<string> ans;
-    
-    void backtrack(string& comb, string next_digits){
-        if(next_digits.empty()){
+
+    void backtrack(string &comb, string next_digits)
+    {
+        if (next_digits.empty())
+        {
             ans.push_back(comb);
-        }else{
-            for(const char& c : digit2letters[next_digits[0]-'0']){
+        }
+        else
+        {
+            for (const char &c : digit2letters[next_digits[0] - '0'])
+            {
                 comb += c;
                 backtrack(comb, next_digits.substr(1));
                 comb.pop_back();
             }
         }
     }
-    
-    vector<string> letterCombinations(string digits) {
+
+    vector<string> letterCombinations(string digits)
+    {
         digit2letters = vector<string>(10);
         digit2letters[2] = "abc";
         digit2letters[3] = "def";
@@ -65,12 +78,13 @@ public:
         digit2letters[7] = "pqrs";
         digit2letters[8] = "tuv";
         digit2letters[9] = "wxyz";
-        
-        if(digits.empty()) return vector<string>();
-        
+
+        if (digits.empty())
+            return vector<string>();
+
         string comb = "";
         backtrack(comb, digits);
-        
+
         return ans;
     }
 };
