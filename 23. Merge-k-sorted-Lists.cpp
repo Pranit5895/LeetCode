11cf3624e9430ef491842f8aaec70e5fb0e8c515
Pranit5895 <guptapranit9@gmail.com>
2021-07-18 18:21:10 +0530

    Approach 2

--- 23. Merge-k-sorted-Lists.cpp
+++ 23. Merge-k-sorted-Lists.cpp
@@ -50,3 +50,33 @@ public:
         return head->next;
     }
 };
+
+//Approach 4: Merge lists one by one
+//extended from "21. Merge Two Sorted Lists"
+//Runtime: 248 ms, faster than 14.96% of C++ online submissions for Merge k Sorted Lists.
+//Memory Usage: 12.1 MB, less than 61.91% of C++ online submissions for Merge k Sorted Lists.
+//time: O(KN), space: O(1)
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == nullptr) return l2;
+        if(l2 == nullptr) return l1;
+        if(l1->val <= l2->val){
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }else{
+            l2->next = mergeTwoLists(l2->next, l1);
+            return l2;
+        }
+    };
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode *head = nullptr;
+        
+        for(int i = 0; i < lists.size(); ++i){
+            head = mergeTwoLists(head, lists[i]);
+        }
+        
+        return head;
+    }
+};
