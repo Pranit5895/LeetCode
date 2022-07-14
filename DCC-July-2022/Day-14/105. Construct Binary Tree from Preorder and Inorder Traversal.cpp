8463869f01762cee33c070b7c6e6a2eb39c0913d
Pranit5895 <53489662+Pranit5895@users.noreply.github.com>
2022-07-14 13:17:42 +0530

    Solution Day-14

---
+++ DCC-July-2022/Day-14/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> inorder_indices;
+    
+    TreeNode* buildTree(vector<int>& preorder, int prestart, int preend, 
+        vector<int>& inorder, int instart, int inend){
+        //[prestart, preend], [inorder, inend] are inclusive
+        if(prestart > preend) return nullptr;
+        
+        TreeNode* root = new TreeNode(preorder[prestart]);
+        
+        // int split = find(inorder.begin()+instart, 
+        //     inorder.begin()+inend+1, root->val)-inorder.begin();
+        int split = inorder_indices[root->val];
+        int left_size = split - instart;
+        
+        root->left = buildTree(preorder, prestart+1, prestart+1+left_size-1, 
+                               inorder, instart, split-1);
+        
+        root->right = buildTree(preorder, prestart+left_size+1, preend, 
+                                inorder, split+1, inend);
+        
+        return root;
+    }
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        
+        for(int i = 0; i < n; ++i){
+            inorder_indices[inorder[i]] = i;
+        }
+        
+        return buildTree(preorder, 0, n-1, inorder, 0, n-1);
+    }
+};
